c7f80e8c4953f3873e42a690d43d177960806ea7
tagjune12 <tagjune12@gmail.com>
2021-08-24 14:13:37 +0900

    점심나가서먹을거같아
    점심나가서먹을거같아 점심나가서먹을거같아 점심나가서먹을거같아

---
+++ 병합모듈테스트/detect_feature.py
@@ -0,0 +1,37 @@
+import mediapipe as mp
+import cv2
+from insukfile import drawing_utils as drawing
+
+
+class FeatureDetector:
+    def __init__(self):
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing = drawing
+        self.mp_holistic = mp.solutions.holistic
+    def detectFeaturePoints(self, frame, image_start:list, image_end:list):
+        # mp_drawing = self.mp_drawing
+        mp_holistic = self.mp_holistic
+
+        for i in range(len(image_start)):
+            print('Log from detectFeaturePoints')
+            start = tuple(image_start[i])
+            end = tuple(image_end[i])
+            # image -> human 영역만 crop 된 이미지
+            image = frame[start[1]:end[1], start[0]:end[0]]  # 이미지[Y좌표, X좌표]
+            # Convert the BGR image to RGB before processing.
+            with mp_holistic.Holistic(min_detection_confidence=0.4, min_tracking_confidence=0.5) as holistic:
+                results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+                '''
+                annotated_image = image.copy()  # 찾은 사람 이미지
+                mp_drawing.draw_landmarks(
+                    annotated_image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+                mp_drawing.draw_landmarks(
+                    annotated_image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+                # frame -> 아무것도 그려지지 않은 이미지
+                # 그 위에 선이나 점같은게 그려진 이미지를 그리는 부분
+                frame[start[1]:end[1], start[0]:end[0]] = annotated_image
+                '''
+
+
+        # return frame
+        return results
---
+++ 병합모듈테스트/display.py
@@ -0,0 +1,123 @@
+import cv2
+import numpy as np
+import time
+import detect_feature
+import detect_person
+import screencapture
+import classification_module as clf
+
+from multiprocessing import Process, Semaphore, shared_memory
+
+class Display():
+    def __init__(self, name, semaphore):
+        self.check = True
+        self.shared_data = tuple()
+        self.shm_name = name
+        self.semaphore = semaphore
+
+    def analysize(self):
+        captureBoard = screencapture.CaptureBoard()
+        peopleDetector = detect_person.PeopleDetector()
+        featureDetector = detect_feature.FeatureDetector()
+        start = time.time()
+        frame_count = 0
+        result = None
+        while True:
+            frame_count += 1
+
+            # self.semaphore.acquire()
+            exist_shm = shared_memory.SharedMemory(name=self.shm_name)
+            capture_size = np.ndarray((4,), dtype=np.int32, buffer = exist_shm.buf)
+            captureBoard.set_capture_size(tuple(capture_size))
+
+            # self.semaphore.release()
+
+            capture_frame = captureBoard.captureScreen()  # 화면 캡처
+            capture_frame = cv2.cvtColor(capture_frame, cv2.COLOR_BGR2RGB)
+            flag, image_start, image_end = peopleDetector.detect(capture_frame)  # 사람 감지
+
+            if flag and len(np.squeeze(image_start)) != 0:
+                # capture_frame = featureDetector.detectFeaturePoints(capture_frame, image_start, image_end)  # 특징점 검출
+                features = featureDetector.detectFeaturePoints(capture_frame, image_start, image_end)  # 특징점 검출
+                result = clf.classify(features)
+
+            # 화면에 표시
+
+            '''
+            cv2.FONT_HERSHEY_SIMPLEX : 0
+            cv2.FONT_HERSHEY_PLAIN : 1
+            cv2.FONT_HERSHEY_DUPLEX : 2
+            cv2.FONT_HERSHEY_COMPLEX : 3
+            cv2.FONT_HERSHEY_TRIPLEX : 4
+            cv2.FONT_HERSHEY_COMPLEX_SMALL : 5
+            cv2.FONT_HERSHEY_SCRIPT_SIMPLEX : 6
+            cv2.FONT_HERSHEY_SCRIPT_COMPLEX : 7
+            cv2.FONT_ITALIC : 16
+
+            [참고]
+            https://copycoding.tistory.com/151
+            '''
+            if result == None:
+                print('Cannot find student')
+                continue
+
+            # cv2.putText(capture_frame,result,image_start,1,15,(0,0,255),10) # 에러발생 local variable 'clf_result' referenced before assignment
+            # capture_frame = cv2.resize(capture_frame, (960, 540))
+            # cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
+            # cv2.imshow("result", capture_frame)
+            end = time.time()
+            print(f"Time Lapse: {frame_count / (end - start)} ")
+            # if cv2.waitKey(1) & 0xFF == ord('q'): break
+            if cv2.waitKey(1) :
+                if not self.check:
+                    print("Flag: ", self.check)
+                    cv2.destroyWindow("result")
+                    break
+
+    # def analysize(self):
+    #     captureBoard = screencapture.CaptureBoard()
+    #     peopleDetector = detect_person.PeopleDetector()
+    #     featureDetector = detect_feature.FeatureDetector()
+    #     start = time.time()
+    #     frame_count = 0
+    #     result = None
+    #     while True:
+    #         frame_count += 1
+    #
+    #         capture_frame = captureBoard.captureScreen()  # 화면 캡처
+    #         capture_frame = cv2.cvtColor(capture_frame, cv2.COLOR_BGR2RGB)
+    #         flag, image_start, image_end = peopleDetector.detect(capture_frame)  # 사람 감지
+    #
+    #         if flag and len(np.squeeze(image_start)) != 0:
+    #             # capture_frame = featureDetector.detectFeaturePoints(capture_frame, image_start, image_end)  # 특징점 검출
+    #             features = featureDetector.detectFeaturePoints(capture_frame, image_start, image_end)  # 특징점 검출
+    #             result = clf.classify(features)
+    #
+    #         if result == None:
+    #             print('Cannot find student')
+    #             continue
+    #
+    #         end = time.time()
+    #         print(f"Time Lapse: {frame_count / (end - start)} ")
+    #
+    #         if cv2.waitKey(1):
+    #             if not self.check:
+    #                 print("Flag: ", self.check)
+    #                 cv2.destroyWindow("result")
+    #                 break
+
+    def set_flag(self, arg):
+        self.check = arg
+
+
+'''
+    def start_analysis(self):
+
+
+        # with multiprocessing.Manager() as manager:
+        #     results = manager.list()
+        # queue = Queue()
+        # value = Array('i',[])
+        # work = Process(target=self.analysize, name= '분석모듈', args=(value))
+        # work.start()
+'''
